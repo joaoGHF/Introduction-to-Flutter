4428dcd15f5e5fe934e1bee81d9ab674886acf68
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-11-13 21:31:31 -0300

    Doing class 1

---
+++ aula_1/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World Project'),
+        backgroundColor: const Color.fromRGBO(169, 215, 256, 1),
+        centerTitle: true,
+      ),
+
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('1 texto'),
+            Text('2 textooooooooo'),
+            Text('3 texto'),
+            Text('4 texto'),
+            Text('5 texto'),
+            Text('6 texto'),
+          ],
+        ),
+      ),
+    );
+  }
+}
